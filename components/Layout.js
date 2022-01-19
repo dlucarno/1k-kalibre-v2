cacddb4c018b088dc203944df132fbc702aeac80
Steven Lucarno DODDE <dlucarno@gmail.com>
2022-01-19 21:48:25 +0100

    fix:minimum error

--- components/Layout.js
+++ components/Layout.js
@@ -20,7 +20,7 @@ export default function Layout({
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap" rel="stylesheet" />
