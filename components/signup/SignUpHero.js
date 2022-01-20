a1488ac6b9a9cae48b2b09f69db654684fe92017
Steven Lucarno DODDE <dlucarno@gmail.com>
2022-01-20 23:18:58 +0100

    *

--- components/signup/SignUpHero.js
+++ components/signup/SignUpHero.js
@@ -14,7 +14,7 @@ export default function SignUpHero() {
         }}}
         className="container container--signup-hero">
         <h1 className="t-h1">1K Kalibre - Colorée</h1>
-        <p className="t-body1 t-light-gray signup-hero__body">Le groupe musical 1K Kalibre, dans le but de rester en contact avec ces supporters, a jugé bon de créer un groupe whatsapp afin de permettre à ces super supporter d'être les tous premiers à écouter les nouveautés du groupe et même d'avoir accès à d'autres privilèges. Soyez donc parmis les premiers à écouter le nouveau et premier morceau du groupe intitulé, <b>Colorée</b></p>
+        <p className="t-body1 t-light-gray signup-hero__body">Le groupe musical 1K Kalibre, dans le but de rester en contact avec ces supporters, a jugé bon de créer un groupe whatsapp afin de permettre à ces super supporter d'être les tous premiers à écouter les nouveautés du groupe et même d'avoir accès à d'autres privilèges. Soyez donc parmis les premiers à écouter le nouveau et premier morceau du groupe intitulé, <b>Colorée</b>.</p>
       </motion.div>
     </section>
   )
